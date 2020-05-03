b16bdaf4f61935c14dcc164f8dc3c17850a5b91f
Praveen Puglia <praveenpuglia@gmail.com>
2020-05-03 21:30:22 +0530

    deploy

---
+++ precache-manifest.962b918bc545d78d76ef8d995c17d5f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3816dab723aa9ed47012",
+    "url": "/tambola-board/css/app.03916453.css"
+  },
+  {
+    "revision": "1dd75a3200784511b1a59ce39148f387",
+    "url": "/tambola-board/index.html"
+  },
+  {
+    "revision": "3816dab723aa9ed47012",
+    "url": "/tambola-board/js/app.4f8a7361.js"
+  },
+  {
+    "revision": "c61eeb7ecd660f8abfe5",
+    "url": "/tambola-board/js/chunk-vendors.e5c9b163.js"
+  },
+  {
+    "revision": "2459dee56a693131a60f42f084ccbb3f",
+    "url": "/tambola-board/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/tambola-board/robots.txt"
+  }
+]);
